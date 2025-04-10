a89ea4efa2b905c25a4d192067a26be6306ae1d7
Ranjith <itssrranjith@gmail.com>
2025-04-10 13:01:15 -0400

    fixed log function error

--- test_calculator.py
+++ test_calculator.py
@@ -36,7 +36,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_log_invalid_base(self): # 1 assertion
         # use same technique from test_divide_by_zero\
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             log(1,23)
     #     fill in code
     # ##########################
