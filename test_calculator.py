6f4a36e2dd192ff1063cead1db8a90909280860b
danie <danielkortright521@gmail.com>
2025-04-10 13:04:04 -0400

    hello

--- test_calculator.py
+++ test_calculator.py
@@ -15,11 +15,17 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(multiply(2,3),6)
+        self.assertEqual(multiply(10,4),0)
+        self.assertEqual(multiply(-1,-2),2)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self): # 3 assertions
+        self.assertAlmostEqual(div(6,3),2)
+        self.assertAlmostEqual(div(10,4),2.5)
+
+
+
     # ##########################
 
     ######## Partner 2
@@ -42,22 +48,21 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
+        self.assertEqual(logarithm(2,1),0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3,4),5)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+           square_root(-3)
+        self.assertEqual(square_root(1),1)
+        self.assertEqual(square_root(4),2)
+
+    #########################
 
 # Do not touch this
 if __name__ == "__main__":
