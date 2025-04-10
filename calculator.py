0e929b32dc75e97e02f7ea624e7ec328c2802057
danie <danielkortright521@gmail.com>
2025-04-10 12:42:12 -0400

    modified calc

--- calculator.py
+++ calculator.py
@@ -9,6 +9,14 @@ import math
 
 # First example
 <<<<<<< HEAD
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b):
     return a + b
 
@@ -25,13 +33,14 @@ def divide(a, b):
         return a / b
 
 
-
 def logarithm(a, b):
     if a <= 0:
         raise ValueError
     return math.log(b, a)
 
 def exponent(a, b):
+    return a ** b
+
 =======
 import math
 
