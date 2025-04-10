4f47b8d1aef3d950e95c8b3ae53887bad099b85f
Ranjith <itssrranjith@gmail.com>
2025-04-10 12:40:06 -0400

    completed partner 2 of test

--- test_calculator.py
+++ test_calculator.py
@@ -3,11 +3,15 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(0,0),0)
+        self.assertEqual(add(1,3),4)
+        self.assertEqual(add(1345,2948),4293)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(2,3),-1)
+        self.assertEqual(sub(10,4),6)
+        self.assertEqual(sub(200,200),0)
     # ##########################
 
     ######## Partner 1
@@ -19,17 +23,21 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
+    def test_divide_by_zero(self): # 1 assertion
+        # call division function inside, example:
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
     #     fill in code
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(10,1000),3)
+        self.assertEqual(log(2,16),4)
+        self.assertEqual(log(2,1),0)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
+    def test_log_invalid_base(self): # 1 assertion
+        # use same technique from test_divide_by_zero\
+        with self.assertRaises(TypeError):
+            log(1,23)
     #     fill in code
     # ##########################
     
